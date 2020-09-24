994cf0a7379833b651a91f7afbf3dccb274409d2
Patryk <patryk.bytow@wp.pl>
2020-09-24 21:29:08 +0200

    E-mail post share opition

--- mysite/blog/views.py
+++ mysite/blog/views.py
@@ -2,6 +2,8 @@ from django.shortcuts import render, get_object_or_404
 from .models import Post
 from django.core.paginator import Paginator, EmptyPage, PageNotAnInteger
 from django.views.generic import ListView
+from .forms import EmailPostForm
+from django.core.mail import send_mail
 
 
 def post_list(request):
@@ -42,3 +44,28 @@ class PostListView(ListView):
     paginate_by = 3
     template_name = 'blog/post/list.html'
 
+
+def post_share(request, post_id):
+    # get post with it's id
+    post = get_object_or_404(Post, id=post_id, status='published')
+    sent = False
+
+    if request.method == 'POST':
+        # form has been sent
+        form = EmailPostForm(request.POST)
+        if form.is_valid():
+            # form has been filled correctly
+            cd = form.cleaned_data
+            # data can be send
+            post_url = request.build_absolute_uri(post.get_absolute_url())
+            subject = '{} ({}) encourage you to read "{}"'.format(cd['name'], cd['email'], post.title)
+            message = 'Please read post "{}" on {}\n\n Comment added by {}: {}'.format(
+                post.title, post_url, cd['name'], cd['comments'])
+            send_mail(subject, message, 'admin@myblog.com', [cd['to']])
+            sent = True
+
+    else:
+        form = EmailPostForm()
+    return render(request, 'blog/post/share.html', {'post': post,
+                                                    'form': form,
+                                                    'sent': sent})
--- mysite/mysite/settings.py
+++ mysite/mysite/settings.py
@@ -100,3 +100,11 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/3.1/howto/static-files/
 
 STATIC_URL = '/static/'
+
+EMAIL_HOST = 'smtp.gmail.com'
+EMAIL_HOST_USER = 'patryk.bytow@gmail.com'
+EMAIL_HOST_PASSWORD = 'test'
+EMAIL_PORT = 587
+EMAIL_USE_TLS = True
+
+# EMAIL_BACKEND = 'django.core.mail.backends.console.EmailBackend'
